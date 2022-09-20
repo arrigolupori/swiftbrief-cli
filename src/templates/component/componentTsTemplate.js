b0347d992c418c4668d9787ad4f38de840527010
Arrigo Lupori <arrigo.lupori@gmail.com>
2022-09-20 16:59:15 +0200

    🔖 (version): 1.2.3
    fixed props destructuring

--- src/templates/component/componentTsTemplate.js
+++ src/templates/component/componentTsTemplate.js
@@ -2,14 +2,13 @@ module.exports = `import { Box, BoxProps, forwardRef } from '@chakra-ui/react'
 
 export interface TemplateNameProps extends BoxProps {
 	name?: string
-	children?: ReactNode
 }
 
 export const TemplateName = forwardRef<TemplateNameProps, 'div'>(
 	(props, ref): any => {
 		console.log('TemplateName')
 		return (
-			<Box data-testid={props.name} ref={ref}>
+			<Box data-testid={props.name} ref={ref} {...props}>
 				{props.children}
 			</Box>
 		)
